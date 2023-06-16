c089cd9d277aa47bb91461c326372f8b4e6b0ce1
redhawkeye <blackstingray@protonmail.com>
2023-06-16 11:05:34 +0700

    Fri Jun 16 11:05:34 WIB 2023

--- startup.sh
+++ startup.sh
@@ -4,6 +4,7 @@ apt update
 apt -y install ca-certificates curl gnupg lsb-release
 mkdir -p /etc/apt/keyrings
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+chmod a+r /etc/apt/keyrings/docker.gpg
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" | tee /etc/apt/sources.list.d/docker.list
 apt update
 DEBIAN_FRONTEND=noninteractive apt -yq install git docker-ce docker-ce-cli containerd.io docker-compose-plugin docker-compose
